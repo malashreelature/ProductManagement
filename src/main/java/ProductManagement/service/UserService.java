9b95448f58622f9550518eaf79d9d48a476a4f99
Malashree Lature <malashreelature22@gmail.com>
2022-11-02 17:58:38 -0700

    Userservice

--- src/main/java/ProductManagement/service/UserService.java
+++ src/main/java/ProductManagement/service/UserService.java
@@ -5,21 +5,23 @@ import ProductManagement.service.exception.BadCredentialsException;
 import ProductManagement.service.exception.UserNotFoundException;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserService {
 
     void saveUser(User user);
 
 
-    User update(User user,Long id);
 
-    List<User> findAll(String username, String password)
+    List<User> findAll(String email, String password)
             throws UserNotFoundException, BadCredentialsException;
 
-    User getById(Long id);
+    User getById(String email,String password,Long wallet)throws UserNotFoundException,
+            BadCredentialsException;;
 
-    void delete(Long id)throws RuntimeException;
+    String deleteById(String email, String password)throws  UserNotFoundException,
+    BadCredentialsException;
 
 
+    User findById(String email, String password, User user)throws UserNotFoundException,
+            BadCredentialsException;
 }
