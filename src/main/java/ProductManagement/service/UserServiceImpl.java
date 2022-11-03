83e22dcd8a2f56481f8f3f6c08ba3edc44f1fca7
Malashree Lature <malashreelature22@gmail.com>
2022-11-02 17:59:24 -0700

    UserServiceImpl

--- src/main/java/ProductManagement/service/UserServiceImpl.java
+++ src/main/java/ProductManagement/service/UserServiceImpl.java
@@ -23,46 +23,57 @@ public class UserServiceImpl implements UserService {
 
     }
 
-    @Override
-    public User update(User user,Long id) {
-        User user1 = this.userRepository.findById(id).orElseThrow(
-                () -> new RuntimeException(" Not Found"));
-        user1.setId(user.getId());
-        user1.setName(user.getName());
-        user1. setId(user.getId());
-        this.userRepository.save(user1);
-        User save = this.userRepository.save(user1);
-        return user;
-    }
 
     @Override
-    public List<User> findAll(String username, String password) throws UserNotFoundException,
+    public User findById(String email, String password,User user) throws
+            UserNotFoundException, BadCredentialsException {
+        User userFromDb =  userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException
+                (String.format("User with username %s not found", email)));
+        System.out.println("User from db: "+userFromDb);
+        if (!userFromDb.getPassword().equals(password)) throw new BadCredentialsException("Bad Credentials");
+        user.setId(userFromDb.getId());
+
+       return userRepository.save(user);
+    }
+
+
+    @Override
+    public List<User> findAll(String email, String password) throws UserNotFoundException,
             BadCredentialsException {
-        User user = (User) UserRepository.findByUsername(username).orElseThrow(() -> new UserNotFoundException
-                (String.format("User with username %s not found", username)));
-        if(!user.getPassword().equals(password)) throw new BadCredentialsException("Bad Credentials");
+        User user =  userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException
+                (String.format("User with username %s not found", email)));
+        if (!user.getPassword().equals(password)) throw new BadCredentialsException("Bad Credentials");
         return userRepository.findAll();
     }
 
-
-
     @Override
-    public User getById(Long id) {
-        User user=this.userRepository.findById(id).orElseThrow(
-                ()->new RuntimeException("Not Found"));
-        return user;
+    public User getById(String email, String password,Long wallet) throws UserNotFoundException,
+            BadCredentialsException {
+        User user =  userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException
+                (String.format("User with username %s not found", email)));
+        if (!user.getPassword().equals(password)) throw new BadCredentialsException("Bad Credentials");
+        user.setWallet(wallet);
+        return userRepository.save(user);
     }
 
     @Override
-    public void delete(Long id) {
-        User user=this.userRepository.findById(id).orElseThrow(
-                ()-> new RuntimeException("Not found"));
-        this.userRepository.delete(user);
-
+    public String deleteById(String email, String password) throws UserNotFoundException,
+            BadCredentialsException {
+        User user =  userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException
+                (String.format("User with username %s not found", email)));
+        if (!user.getPassword().equals(password)) throw new BadCredentialsException
+                ("Bad Credentials");
+        userRepository.deleteById(user.getId());
+        return "Delete by id sucessfully";
     }
-
 
 
 
 }
+
+
+
+
+
+
 
